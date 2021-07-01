5ab2d6ec20ae2d4b76778c3acc4141281c258944
davislatiss <davis.latiss@gmail.com>
2021-07-01 20:36:04 +0300

    edits

--- Requests/AccountTransactionsRequest.py
+++ Requests/AccountTransactionsRequest.py
@@ -1,14 +1,14 @@
-import requests
 import json
+import requests
 
 url = "https://api-sandbox.sebgroup.com/ais/v6/identified2/accounts/5a59028c-e757-4f22-b88c-3ba90573383c/transactions?bookingStatus=booked"
 
-
 headers = {
     'Accept': 'application/json',
-    'X-Request-ID': '41151176-1540-4da2-a2bb-3aaffec7eb99',
+    'X-Request-ID': 'a2097523-eb1b-411b-a4e6-0312bdc5b1a9',
     'PSU-IP-Address': '129.178.88.88',
-    'Cookie': 'C0WNET=77b97511-60dc-4ec7-a30b-29cb2bba34a0; TS0111c347=0107224bedea62dc62f59d862fde90c6f01f56f68a26a0fedef76dc6e1ab55cba483a0d8c45ec547a60dd9a15559c1ec0ffb001aae; AMWEBJCT!%2Fmga!JSESSIONID=0000II33GVlcgFrwWYZobgRYmIy:938ed943-3c20-4801-ace0-52dd1dd31f86; PD-SESSION-ID=1_4_1_DdHDBMnT-BYpeeC4IT9S0IiwsC7qzbOBD0UmxCgS99IMMrra; PGNET=77b97511-60dc-4ec7-a30b-29cb2bba34a0; TS01136fc4=0107224bed0c55a73fe49e4bdb7b3bf6675c2e6d18e855b5e9417c33bf0b157b8ac58951f65462ed41fee8e81701dda5e9d9c5e50a; TS018787d1=0107224bed94c812f71a30bc2701042242fb7b7ae40b44654ca6072a0187d9aa7236aae4fd6a1b6128efe18b1fdf46153540dedbec'
+    'Authorization': 'Bearer mviOec9YH6u6YNUB05j4',
+    'Cookie': 'C0WNET=77b97511-60dc-4ec7-a30b-29cb2bba34a0; AMWEBJCT!%2Fmga!JSESSIONID=0000F53HcDLyrSgxfjqOXKQCZvo:938ed943-3c20-4801-ace0-52dd1dd31f86; PD-SESSION-ID=1_4_1_CbJOrE7hxWz22nqGObKz4jfSVo2Un4rTMr7fCOaYJusbe66J; PGNET=7a2efed9-60dd-4973-a235-f03518dcac86; TS01136fc4=0107224bed324d42d21e39fecb560406af42106fdab2a8e9499028abdb56ed03b6c27a7c324e0dd371e4b9450d7d47515c22c20167; TS018787d1=0107224bed4d939241909a766fa271b066eb277d22e00f0ca914ae43033577b6771af34b348e4caff80c89a5c5e2bb7abdbc860609'
 }
 
 response = requests.request("GET", url, headers=headers)
--- Requests/AccountsRequest.py
+++ Requests/AccountsRequest.py
@@ -1,16 +1,19 @@
-import requests
 import json
+import requests
 
 url = "https://api-sandbox.sebgroup.com/ais/v6/identified2/accounts"
 
 
 headers = {
     'Accept': 'application/json',
-    'X-Request-ID': '41151176-1540-4da2-a2bb-3aaffec7eb99',
+    'X-Request-ID': '16e31382-a539-4a27-9053-672a5c4079b7',
     'PSU-IP-Address': '129.178.88.88',
-    'Cookie': 'C0WNET=77b97511-60dc-4ec7-a30b-29cb2bba34a0; TS0111c347=0107224bedea62dc62f59d862fde90c6f01f56f68a26a0fedef76dc6e1ab55cba483a0d8c45ec547a60dd9a15559c1ec0ffb001aae; AMWEBJCT!%2Fmga!JSESSIONID=0000II33GVlcgFrwWYZobgRYmIy:938ed943-3c20-4801-ace0-52dd1dd31f86; PD-SESSION-ID=1_4_1_DdHDBMnT-BYpeeC4IT9S0IiwsC7qzbOBD0UmxCgS99IMMrra; PGNET=77b97511-60dc-4ec7-a30b-29cb2bba34a0; TS01136fc4=0107224bed0c55a73fe49e4bdb7b3bf6675c2e6d18e855b5e9417c33bf0b157b8ac58951f65462ed41fee8e81701dda5e9d9c5e50a; TS018787d1=0107224bed94c812f71a30bc2701042242fb7b7ae40b44654ca6072a0187d9aa7236aae4fd6a1b6128efe18b1fdf46153540dedbec'
+    'Authorization': 'Bearer mviOec9YH6u6YNUB05j4',
+    'Cookie': 'C0WNET=77b97511-60dc-4ec7-a30b-29cb2bba34a0; AMWEBJCT!%2Fmga!JSESSIONID=0000F53HcDLyrSgxfjqOXKQCZvo:938ed943-3c20-4801-ace0-52dd1dd31f86; PD-SESSION-ID=1_4_1_CbJOrE7hxWz22nqGObKz4jfSVo2Un4rTMr7fCOaYJusbe66J; PGNET=7a2efed9-60dd-4973-a235-f03518dcac86; TS01136fc4=0107224bedc99e697684af998c221a8613aa75ad4b200b6e365c90055530327e789f4e73e80f370652908b4fdc497b32f2d41e0648; TS018787d1=0107224bed4d939241909a766fa271b066eb277d22e00f0ca914ae43033577b6771af34b348e4caff80c89a5c5e2bb7abdbc860609'
 }
 
 response = requests.request("GET", url, headers=headers)
 parsed = json.loads(response.text)
 print(json.dumps(parsed, indent=3, separators=(',', ':'), sort_keys=True))
+
+
